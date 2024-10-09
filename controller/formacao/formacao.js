323397d95dd0c5c15d712e19291bd522f6e6e25d
aliciaab <alicia.5959.senac@gmail.com>
2024-10-09 13:50:12 -0300

    amem

--- controller/formacao/formacao.js
+++ controller/formacao/formacao.js
@@ -1,11 +1,5 @@
-// front-end/formacao.js
-import { criarCartoes } from './cartoes.js';
+import { criarCartoes } from "./cartoes.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('http://localhost:3000/api/cartoes')
-        .then(response => response.json())
-        .then(data => {
-            criarCartoes(data);
-        })
-        .catch(error => console.error('Erro ao buscar os cartões:', error));
+  criarCartoes(); // Chama a função após o DOM ser carregado
 });
